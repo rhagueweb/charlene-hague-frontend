f585785c25162f7f18bda96be160a76b8f30e990
Richard Hague <rhagueweb@gmail.com>
2024-02-21 01:43:40 +0000

    Fixing front-end content rendering issue

--- src/blocks/Hero.jsx
+++ src/blocks/Hero.jsx
@@ -1,10 +1,18 @@
+import Image from 'next/image';
 import React from 'react';
 
 export default function Hero({ heading, text, backgroundImage }) {
+  console.log(backgroundImage);
   return (
     <div>
       <h2>{heading}</h2>
       <p>{text}</p>
+      <Image
+        src={backgroundImage.url}
+        height={backgroundImage.height}
+        width={backgroundImage.width}
+        alt={backgroundImage.alt}
+      />
     </div>
   );
 }
--- src/utils/RenderBlocks.jsx
+++ src/utils/RenderBlocks.jsx
@@ -2,15 +2,17 @@ import { blocks } from '@/blocks/blockList';
 import React from 'react';
 
 const RenderBlocks = ({ layout }) => {
-  <div>
-    {layout.map((block, i) => {
-      const Block = blocks[block.blockType];
-      if (Block) {
-        return <Block key={i} {...block} />;
-      }
-      return null;
-    })}
-  </div>;
+  return (
+    <div>
+      {layout.map((block, i) => {
+        const Block = blocks[block.blockType];
+        if (Block) {
+          return <Block key={i} {...block} />;
+        }
+        return null;
+      })}
+    </div>
+  );
 };
 
 export default RenderBlocks;
