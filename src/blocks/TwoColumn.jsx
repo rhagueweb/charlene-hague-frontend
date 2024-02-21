bf5fbecbb36b70e5085790b004f8f1679d54dbf3
Richard Hague <rhagueweb@gmail.com>
2024-02-21 23:09:17 +0000

    Configuring and applying Tailwind

--- src/blocks/TwoColumn.jsx
+++ src/blocks/TwoColumn.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 export default function TwoColumn({ heading, text, image, direction }) {
@@ -5,6 +6,12 @@ export default function TwoColumn({ heading, text, image, direction }) {
     <div>
       <h2>{heading}</h2>
       <p>{text}</p>
+      <Image
+        src={image.url}
+        height={image.height}
+        width={image.width}
+        alt={image.alt}
+      />
     </div>
   );
 }
