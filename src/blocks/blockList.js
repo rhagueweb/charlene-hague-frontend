3256711ddab056fe7c35409ba533e188553c5d95
Richard Hague <rhagueweb@gmail.com>
2024-02-23 22:29:08 +0000

    frontend updates

--- src/blocks/blockList.js
+++ src/blocks/blockList.js
@@ -1,7 +1,9 @@
 import Hero from './Hero';
+import SimpleRichText from './SimpleRichText';
 import TwoColumn from './TwoColumn';
 
 export const blocks = {
   hero: Hero,
   twoColumn: TwoColumn,
+  simpleRichText: SimpleRichText,
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     './src/**/*.{js,ts,jsx,tsx,mdx}',
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/blog-posts/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/blocks/**/*.{js,ts,jsx,tsx,mdx}',
